0b329cc89e89095bbbe70588c299c9c7eb7b2db7
Nayara Simões <93724124+NayaraSimoes@users.noreply.github.com>
2023-05-18 17:52:43 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
+
+// Get all post
+async function getAllPosts() {
+  const response = await fetch(url);
+
+  console.log(response);
+}
+
+getAllPosts();
+
