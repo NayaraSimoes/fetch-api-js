324999e421efc90b760482ed8715c24886748706
Nayara Simões <93724124+NayaraSimoes@users.noreply.github.com>
2023-05-25 10:42:11 -0300

    em andamento
    bugs para corrigir

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ const url = "https://jsonplaceholder.typicode.com/posts";
 const loadingElement = document.querySelector("#loading");
 const postsContainer = document.querySelector("#posts-container");
 
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
 //Get id from URL
 const urlSearchParams = new URLSearchParams(window.location.search);
 const postId = urlSearchParams.get("id");
@@ -38,9 +41,39 @@ async function getAllPosts() {
   });
 }
 
+//Get individual post
+async function getPost(Id) {
+
+  const[responsePost, responseComments] = await Promise.all([
+    fetch(`${url}/${id}`),
+    fetch(`${url}/${id}/comments`),
+  ]);
+
+  const dataPost = await responsePost.json();
+
+  const dataComments = await responseComments.json();
+
+  loadingElement.classList.add("hide");
+  postPage.classList.remove("hide");
+
+  const title = document.createElement("h1");
+  const body = document.createElement("p");
+
+  title.innerText = dataPost.title;
+  body.innerText = dataPost.body;
+
+  postsContainer.appendChild(title);
+  postContainer.appendChild(body);
+
+  console.log(dataComments);
+
+  dataComments.map((comment) => {});
+
+}
+
 if(!postId) {
   getAllPosts();
 } else {
-  console.log(postId);
+  getPost(postId);
 }
 
